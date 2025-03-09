2584916acfd8d732d66ec427c07ea8aef46b7167
Goktug <20567087+goktug97@users.noreply.github.com>
2025-03-09 20:22:26 +0100

    Improve accuracy

--- src/slam.py
+++ src/slam.py
@@ -55,9 +55,6 @@ class Reading:
 class SLAM:
     def __init__(self):
         self.optimizer = pose_graph.PoseGraphOptimization()
-        self.sensors = dict()
-        self.sensor_ids = itertools.count(0)
-        self.readings = []
         self.vertex_counter = itertools.count(0)
         self.prev_odom = g2o.SE2(g2o.Isometry2d(np.eye(3)))
         self.optimizer.add_vertex(next(self.vertex_counter), self.prev_odom, True)
@@ -65,88 +62,87 @@ class SLAM:
         self.registered_scans = dict()
 
     def add_reading(self, reading: Reading):
-        heapq.heappush(self.readings, reading)
+        data = reading.data
 
-    def optimize(self):
-        while len(self.readings):
-            reading = heapq.heappop(self.readings)
-            data = reading.data
+        if isinstance(data, Laser):
+            angles = np.arange(data.angle_min, data.angle_max, data.angle_increment)
+            positions = (
+                np.array([np.cos(angles), np.sin(angles)]).T
+                * data.distances[:, np.newaxis]
+            )
+            mask = np.linalg.norm(positions, axis=1) < data.max_distance
+            positions = positions[mask]
 
-            if isinstance(data, Laser):
-                angles = np.arange(data.angle_min, data.angle_max, data.angle_increment)
-                positions = (
-                    np.array([np.cos(angles), np.sin(angles)]).T
-                    * data.distances[:, np.newaxis]
+            if not len(self.registered_scans):
+                self.registered_scans[self.last_vertex_idx] = positions
+                return
+
+            prev_idx = next(reversed(self.registered_scans.keys()))
+            prev_scan = self.registered_scans[prev_idx]
+            prev_pose = self.optimizer.get_pose(prev_idx)
+            current_pose = self.optimizer.get_pose(self.last_vertex_idx)
+            transform = prev_pose.inverse() * current_pose
+            if (
+                np.linalg.norm(transform.translation()) > 0.4
+                or abs(transform.rotation().angle()) > 0.2
+            ):
+                transformation, distances, iter = icp.icp(
+                    positions,
+                    prev_scan,
+                    transform.to_isometry().matrix(),
+                    max_iterations=80,
+                    tolerance=0.00001,
                 )
-                mask = np.linalg.norm(positions, axis=1) < data.max_distance
-                positions = positions[mask]
-
-                if not len(self.registered_scans):
-                    self.registered_scans[self.last_vertex_idx] = positions
-                    continue
-
-                prev_idx = next(reversed(self.registered_scans.keys()))
-                prev_scan = self.registered_scans[prev_idx]
-                prev_pose = self.optimizer.get_pose(prev_idx)
-                current_pose = self.optimizer.get_pose(self.last_vertex_idx)
-                transform = prev_pose.inverse() * current_pose
-                if (
-                    np.linalg.norm(transform.translation()) > 0.4
-                    or abs(transform.rotation().angle()) > 0.2
-                ):
-                    transformation, distances, iter = icp.icp(
-                        positions,
-                        prev_scan,
-                        transform.to_isometry().matrix(),
-                        max_iterations=80,
-                        tolerance=0.00001,
-                    )
-                    self.optimizer.add_edge(
-                        [prev_idx, self.last_vertex_idx],
-                        g2o.SE2(g2o.Isometry2d(transformation)),
-                        # TODO: How to calculate information matrix?
-                        information=10.0 * np.eye(3),
-                    )
-                    self.registered_scans[self.last_vertex_idx] = positions
-
-                    # Loop closure only makes sense if the last reading is a valid laser scan
-                    self.loop_closure()
-
-            elif isinstance(data, Odometry):
-                current_odom = g2o.SE2(g2o.Isometry2d(data.transform))
-                transform = self.prev_odom.inverse() * current_odom
-
-                if (np.linalg.norm(transform.translation()) < np.finfo(float).eps) and (
-                    transform.rotation().angle() < np.finfo(float).eps
-                ):
-                    continue
-
-                vertex_idx = next(self.vertex_counter)
-                assert data.transform.shape == (3, 3)
-                self.optimizer.add_vertex(vertex_idx, current_odom)
                 self.optimizer.add_edge(
-                    [vertex_idx - 1, vertex_idx],
-                    transform,
-                    information=np.eye(3),
+                    [prev_idx, self.last_vertex_idx],
+                    g2o.SE2(g2o.Isometry2d(transformation)),
+                    # TODO: How to calculate information matrix?
+                    information=100.0 / np.mean(distances) * np.eye(3),
                 )
-                self.last_vertex_idx = vertex_idx
-                self.prev_odom = current_odom
-            else:
-                raise ValueError(f"Unknown data type: {type(data)}")
+                self.registered_scans[self.last_vertex_idx] = positions
 
-            if self.last_vertex_idx > 1:
-                self.optimizer.optimize()
+                # Loop closure only makes sense if the last reading is a valid laser scan
+                self.loop_closure()
+
+        elif isinstance(data, Odometry):
+            current_odom = g2o.SE2(g2o.Isometry2d(data.transform))
+            transform = self.prev_odom.inverse() * current_odom
+
+            if (np.linalg.norm(transform.translation()) < np.finfo(float).eps) and (
+                transform.rotation().angle() < np.finfo(float).eps
+            ):
+                return
+
+            vertex_idx = next(self.vertex_counter)
+            assert data.transform.shape == (3, 3)
+            self.optimizer.add_vertex(vertex_idx, current_odom)
+            self.optimizer.add_edge(
+                [vertex_idx - 1, vertex_idx],
+                transform,
+                information=np.eye(3),
+            )
+            self.last_vertex_idx = vertex_idx
+            self.prev_odom = current_odom
+        else:
+            raise ValueError(f"Unknown data type: {type(data)}")
+
+        if self.last_vertex_idx > 1:
+            self.optimizer.optimize()
 
     def loop_closure(self):
         if len(self.registered_scans) < 2:
             return
 
-        scans = reversed(self.registered_scans.items())
-        current_idx, current_scan = next(scans)
+        scans = enumerate(reversed(self.registered_scans.items()))
+        scan_idx, (current_idx, current_scan) = next(scans)
         assert current_idx == self.last_vertex_idx
         current_pose = self.optimizer.get_pose(current_idx)
 
-        for prev_idx, prev_scan in scans:
+        for prev_scan_idx, (prev_idx, prev_scan) in scans:
+            if prev_scan_idx - scan_idx < 10:
+                # Too recent
+                continue
+
             vertex = self.optimizer.vertex(prev_idx)
             inv_hessian, valid = self.optimizer.compute_marginals(vertex)
 
@@ -179,7 +175,7 @@ class SLAM:
                         [current_idx, prev_idx],
                         g2o.SE2(g2o.Isometry2d(transformation)),
                         robust_kernel=rk,
-                        information=np.eye(3),
+                        information=0.1 / np.mean(distances) * np.eye(3),
                     )
 
 
@@ -264,7 +260,6 @@ slam = SLAM()
 readings = load_clf_from_file(args.input)
 for idx, reading in enumerate(readings):
     slam.add_reading(reading)
-    slam.optimize()
 
     raylib.BeginDrawing()
     raylib.ClearBackground(pyray.WHITE)
